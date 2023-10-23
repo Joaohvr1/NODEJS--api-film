22828197c3002aa773c8177f8ca7dc0788acea74
João Henrique <joaohenriquevidalribeiro@gmail.com>
2023-10-23 14:43:39 -0300

    Project API Finished

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ const app = new express();
 app.use(express.json());
 const port = 3000;
 
-
-mongoose.connect("mongodb+srv://joaohvr1:evr102@start-api.sjcynqs.mongodb.net/?retryWrites=true&w=majority");
-
 const Film = mongoose.model('Film',{
     title: String,
     description: String,
@@ -15,10 +12,28 @@ const Film = mongoose.model('Film',{
 });
 
 
-app.get('/', (req,res) => {
-    res.send("Hello World");
+app.get('/', async (req,res) => {
+    const films = await Film.find();
+    res.send(films);
 })
 
+app.delete('/:id', async (req, res) => {
+    const film = await Film.findByIdAndDelete(req.params.id);
+    res.send(film);
+})
+
+app.put('/:id', async (req,res) => {
+    const film = await Film.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        trailer_url: req.body.trailer_url
+    }, {
+        new: true
+    });
+    
+    return res.send(film);
+})
 app.post('/', async (req,res) => {
 
     const film = new Film({
@@ -29,9 +44,10 @@ app.post('/', async (req,res) => {
     });
 
     await film.save();
-    res.send(film);
+    return res.send(film);
 })
 
 app.listen(port, () => {
+    mongoose.connect("mongodb+srv://joaohvr1:evr102@start-api.sjcynqs.mongodb.net/?retryWrites=true&w=majority");
     console.log(`App rodando em localhost/${port}`);
 })
